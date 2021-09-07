2e5ad1c416a9a09edf2866521de17feeb02ebf39
Justin C Ferguson <jferg@jferg.dev>
2021-09-06 21:48:28 -0500

    Added post-publish cleanup.

--- cleanup.js
+++ cleanup.js
@@ -4,14 +4,9 @@ const { join } = require('path');
 const TARGET = './dist';
 
 fse.readdirSync(TARGET).forEach((file) => {
-  const entry = join(TARGET, file);
   const output = join(__dirname, file);
 
   if (fse.existsSync(output)) {
-    throw new Error(
-      `Can't unwrap dist. File already ${file} exists in root folder.`,
-    );
+    fse.removeSync(output);
   }
-
-  fse.copySync(entry, output);
 });
